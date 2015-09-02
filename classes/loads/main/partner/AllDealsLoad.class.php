0cd5e5118fb24674ca69926771dfa6c2c78071c3
vahagns <vahagn.sookiasian@naghashyan.com>
2015-09-02 09:44:22 +0400

    123

---
+++ classes/loads/main/partner/AllDealsLoad.class.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * @author Levon Naghashyan <levon@naghashyan.com>
+ * @site http://naghashyan.com
+ * @mail levon@naghashyan.com
+ * @year 2010-2015
+ * @package loads.site.main
+ * @version 2.0.0
+ */
+
+namespace crm\loads\main\partner {
+
+    use crm\loads\NgsLoad;
+    use crm\managers\CalculationManager;
+    use crm\managers\CurrencyManager;
+    use crm\managers\PartnerManager;
+    use crm\managers\PaymentTransactionManager;
+    use crm\managers\PurchaseOrderManager;
+    use crm\managers\SaleOrderManager;
+    use crm\security\RequestGroups;
+    use NGS;
+
+    class AllDealsLoad extends NgsLoad {
+
+        public function load() {
+            $this->initErrorMessages();
+            $this->initSuccessMessages();
+            $partnerId = intval(NGS()->args()->id);
+            $partner = PartnerManager::getInstance()->selectbyPK($partnerId);
+            if ($partner) {
+                $this->addParam('partner', $partner);
+                $partnerSaleOrders = SaleOrderManager::getInstance()->getPartnerSaleOrders($partnerId);
+                $partnerPurchaseOrders = PurchaseOrderManager::getInstance()->getPartnerPurchaseOrders($partnerId);
+                $partnerPaymentTransactions = PaymentTransactionManager::getInstance()->getPartnerPaymentTransactions($partnerId);
+                $partnerBillingTransactions = PaymentTransactionManager::getInstance()->getPartnerBillingTransactions($partnerId);
+                $this->addParam('partnerSaleOrders', $partnerSaleOrders);
+                $this->addParam('partnerPurchaseOrders', $partnerPurchaseOrders);
+                $this->addParam('partnerPaymentTransactions', $partnerPaymentTransactions);
+                $this->addParam('partnerBillingTransactions', $partnerBillingTransactions);
+            }
+        }
+        
+        private function mergeAllDeals($partnerSaleOrders, $partnerPurchaseOrders, $partnerPaymentTransactions, $partnerBillingTransactions)
+        {
+            $ret = [];
+            
+        }
+        private function mapByIdAndDate($partnerTransactions)
+        {
+            $ret = [];
+            foreach ($partnerTransactions as $partnerTransaction) {
+                $ret [] = $partnerTransaction->getDate();
+            }
+        }
+
+        public function getTemplate() {
+            return NGS()->getTemplateDir() . "/main/partner/open.tpl";
+        }
+
+        public function getRequestGroup() {
+            return RequestGroups::$guestRequest;
+        }
+
+    }
+
+}
