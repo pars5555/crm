b8dd9fed4c861c4e72406143ac3ee324568be823
vahagns <vahagn.sookiasian@naghashyan.com>
2016-05-19 11:09:57 +0400

    123

--- classes/actions/main/purchase/SavePurchaseOrderLinesAction.class.php
+++ classes/actions/main/purchase/SavePurchaseOrderLinesAction.class.php
@@ -15,6 +15,7 @@
 namespace crm\actions\main\purchase {
 
     use crm\actions\BaseAction;
+    use crm\exceptions\InsufficientProductException;
     use crm\managers\PurchaseOrderLineManager;
     use crm\managers\PurchaseOrderManager;
     use NGS;
@@ -27,25 +28,41 @@ namespace crm\actions\main\purchase {
                 $this->redirect('purchase/list');
             }
             $purchaseOrderId = intval(NGS()->args()->purchase_order_id);
-            if (isset(NGS()->args()->lines)) {
-                $jsonLinesArray = NGS()->args()->lines;
-                $linesIdsToNotDelete = [];
-                if (!empty($jsonLinesArray)) {
-                    foreach ($jsonLinesArray as $jsonLine) {
-                        $line = json_decode($jsonLine);
-                        if (isset($line->line_id)) {
-                            $linesIdsToNotDelete[] = $line->line_id;
-                            PurchaseOrderLineManager::getInstance()->updatePurchaseOrderLine($purchaseOrderId, $line->line_id, $line->product_id, $line->quantity, $line->unit_price, $line->currency_id);
-                        } else {
-                            $newLineId = PurchaseOrderLineManager::getInstance()->createPurchaseOrderLine($purchaseOrderId, $line->product_id, $line->quantity, $line->unit_price, $line->currency_id);
-                            $linesIdsToNotDelete[] = $newLineId;
+            try {
+                if (isset(NGS()->args()->lines)) {
+                    $jsonLinesArray = NGS()->args()->lines;
+                    $linesIdsToNotDelete = [];
+                    if (!empty($jsonLinesArray)) {
+                        foreach ($jsonLinesArray as $jsonLine) {
+                            $line = json_decode($jsonLine);
+                            if (isset($line->line_id)) {
+                                $linesIdsToNotDelete[] = $line->line_id;
+                                PurchaseOrderLineManager::getInstance()->updatePurchaseOrderLine($purchaseOrderId, $line->line_id, $line->product_id, $line->quantity, $line->unit_price, $line->currency_id);
+                            } else {
+                                $newLineId = PurchaseOrderLineManager::getInstance()->createPurchaseOrderLine($purchaseOrderId, $line->product_id, $line->quantity, $line->unit_price, $line->currency_id);
+                                $linesIdsToNotDelete[] = $newLineId;
+                            }
                         }
                     }
+                    PurchaseOrderLineManager::getInstance()->deleteWhereIdNotIdIds($purchaseOrderId, $linesIdsToNotDelete);
+                } else {
+                    PurchaseOrderLineManager::getInstance()->deleteByField('purchase_order_id', $purchaseOrderId);
                 }
-                PurchaseOrderLineManager::getInstance()->deleteWhereIdNotIdIds($purchaseOrderId, $linesIdsToNotDelete);
-            } else {
-                PurchaseOrderLineManager::getInstance()->deleteByField('purchase_order_id', $purchaseOrderId);
+            } catch (InsufficientProductException $exc) {
+                SaleOrderManager::getInstance()->rollbackTransaction();
+                $product = \crm\managers\ProductManager::getInstance()->selectByPK($exc->getProductId());
+                $productInfo = $product->getId();
+                if (isset($product)) {
+                    $productInfo = $product->getName() . " (" . $product->getId() . ")";
+                }
+                $_SESSION['error_message'] = "Insufficient Product: " . $productInfo;
+                $this->redirect('purchase/' . $purchaseOrderId);
+            } catch (Exception $exc) {
+                SaleOrderManager::getInstance()->rollbackTransaction();
+                $_SESSION['error_message'] = $exc->getMessage();
+                $this->redirect('purchase/' . $purchaseOrderId);
             }
+
             $_SESSION['success_message'] = 'Purchase Order lines successfully saved.';
             PurchaseOrderManager::getInstance()->updateAllDependingSaleOrderLines($purchaseOrderId);
             $this->redirect('purchase/warranty/' . $purchaseOrderId);
