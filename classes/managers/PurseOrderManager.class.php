a8c23a884c2bd5cb58493fa9c16482605ccbc6e2
Vahagn Sookiasian <pars5555@yahoo.com>
2018-10-14 23:06:16 +0400

    123

--- classes/managers/PurseOrderManager.class.php
+++ classes/managers/PurseOrderManager.class.php
@@ -56,7 +56,7 @@ namespace crm\managers {
                 }
             }
             $unitAddressSql = "('" . implode("','", array_keys($partnerIdMappedByExpressUnitAddresses)) . "')";
-            $orders = $this->selectAdvance('*', ['status', '<>', "'canceled'", 'AND', 'unit_address', 'in', $unitAddressSql, 'AND', 'ABS(DATEDIFF(`created_at`, date(now())))', '<=', 50]);
+            $orders = $this->selectAdvance('*', ['status', '<>', "'canceled'", 'AND', 'unit_address', 'in', $unitAddressSql, 'AND', 'ABS(DATEDIFF(`created_at`, date(now())))', '<=', 50], 'id', 'desc');
             $recipientsRecentOrdersMappedByrecipientId = [];
             foreach ($orders as $order) {
                 $expressUnitAddress = $order->getUnitAddress();
