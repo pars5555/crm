fb4d07de08d0f67ec2f54173e6478ae611acb1b3
Vahagn Sookiasian <pars5555@yahoo.com>
2018-10-29 11:04:43 +0400

    123

--- classes/loads/main/general/GeneralLoad.class.php
+++ classes/loads/main/general/GeneralLoad.class.php
@@ -33,10 +33,10 @@ namespace crm\loads\main\general {
             $partnerWarehouseTotal = $this->getPartnerWarehausesTotalAmount();
             
             $this->addParam('partnerDebtTotal', $partnerDebtTotal);
-            $this->addParam('$warehouseTotal', $warehouseTotal);
-            $this->addParam('$purseTotal', $purseTotal);
-            $this->addParam('$purseBalanceTotal', $purseBalanceTotal);
-            $this->addParam('$partnerWarehouseTotal', $partnerWarehouseTotal);
+            $this->addParam('warehouseTotal', $warehouseTotal);
+            $this->addParam('purseTotal', $purseTotal);
+            $this->addParam('purseBalanceTotal', $purseBalanceTotal);
+            $this->addParam('partnerWarehouseTotal', $partnerWarehouseTotal);
             $this->addParam('capital', $warehouseTotal + $purseTotal + $purseBalanceTotal + $partnerWarehouseTotal + $partnerDebtTotal);
         }
 
--- templates/main/general/general.tpl
+++ templates/main/general/general.tpl
@@ -4,11 +4,11 @@
     {/literal} 
     <h1 class="main_title">General</h1>
     <h1>Capital: {$ns.capital|number_format:2}<h1/>  
-        partnerDebtTotal: {$partnerDebtTotal}, 
-        warehouseTotal: {$warehouseTotal}, 
-        btcProductsTotal, {$purseTotal},
-        btcBalancesTotal, {$purseBalanceTotal},
-        partnerWarehouseTotal, {$partnerWarehouseTotal})<br/><br/><br/>
+        partnerDebtTotal: {$partnerDebtTotal|number_format:2}, 
+        warehouseTotal: {$warehouseTotal|number_format:2}, 
+        btcProductsTotal, {$purseTotal|number_format:2},
+        btcBalancesTotal, {$purseBalanceTotal|number_format:2},
+        partnerWarehouseTotal, {$partnerWarehouseTotal|number_format:2})<br/><br/><br/>
     <div id="cashboxCalculationContainer">
         {nest ns=cashboxCalculation}
     </div>
