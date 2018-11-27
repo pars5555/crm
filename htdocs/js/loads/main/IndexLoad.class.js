c75e1b9c6eec0246ad20d8c65a554e65d5038ef2
Vahagn <pars5555@yahoo.com>
2018-11-27 16:02:27 +0400

    123

--- htdocs/js/loads/main/IndexLoad.class.js
+++ htdocs/js/loads/main/IndexLoad.class.js
@@ -55,7 +55,7 @@ NGS.createLoad("crm.loads.main.index", {
         });
     },
     initSettingEditableCells: function () {
-        $('.f_editable_setting_field').dblclick(function () {
+        $(document).on('dblclick', '.f_editable_setting_field', function () {
             var cellValues = $(this).text().trim();
             var cellFieldName = $(this).data('field-name');
             var type = $(this).data('type');
