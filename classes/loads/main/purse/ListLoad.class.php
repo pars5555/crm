186dc5066444f59c84ca44a205ba3404b5a21af4
Vahagn Sookiasian <pars5555@yahoo.com>
2018-12-14 01:30:46 +0400

    123

--- classes/loads/main/purse/ListLoad.class.php
+++ classes/loads/main/purse/ListLoad.class.php
@@ -93,6 +93,7 @@ namespace crm\loads\main\purse {
                 $this->addParam('info_btc_address', $purse_info_meta->wallet->BTC->legacy_address);
             }
             $external_products_days_diff_for_created_date = intval(\crm\managers\SettingManager::getInstance()->getSetting('external_products_days_diff_for_created_date'));
+            $this->addParam('external_products_days_diff_for_created_date', $external_products_days_diff_for_created_date);
 
 
             $parsUpdatedDate = PurseOrderManager::getInstance()->getAccountUpdatedDateString('purse_pars');
