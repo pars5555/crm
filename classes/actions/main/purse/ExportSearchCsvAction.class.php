1ecd99b9316965aa723fbccd0d26f7c32a49314a
Vahagn Sookiasian <pars5555@yahoo.com>
2018-12-24 02:05:03 +0400

    123

--- classes/actions/main/purse/ExportSearchCsvAction.class.php
+++ classes/actions/main/purse/ExportSearchCsvAction.class.php
@@ -12,19 +12,24 @@ namespace crm\actions\main\purse {
             $trackingNumbersStr = str_replace(',', ';', $trackingNumbersStr);
             $trackingNumbersArray = explode(";", $trackingNumbersStr);
 
-            $rows = \crm\managers\PurseOrderManager::getInstance()->findByTrackingNumbers($trackingNumbersArray);
-            $this->exportCsv($rows);
+            $orders = \crm\managers\PurseOrderManager::getInstance()->findByTrackingNumbers($trackingNumbersArray);
+            $this->exportCsv($orders);
         }
 
-        public function exportCsv($rows) {
+        public function exportCsv($orders) {
             header('Content-type: text/csv; charset=UTF-8');
             header('Content-Disposition: attachment; filename=export.csv');
             $output = fopen('php://output', 'w');
             fputs($output, chr(0xEF) . chr(0xBB) . chr(0xBF));
            
             fputcsv($output, ['Given Tracking Number', 'Order Tracking Number', 'Recipient', 'Qty', 'Product', 'Total $']);
-            foreach ($rows as $tr => $row) {
-                fputcsv($output, ['"'.$tr.'"','"'. $row->getTrackingNumber().'"', $row->getRecipientName(), $row->getQuantity(), $row->getProductName(), $row->getAmazonTotal()]);
+            
+            foreach ($orders as $tr => $order) {
+                $row = ['"'.$tr.'"','"'. $order->getTrackingNumber().'"', $order->getRecipientName(), $order->getQuantity(), $order->getProductName(), $order->getAmazonTotal()];
+                $row = array_map(function(&$el){
+                    return '="'.$el .'"';
+                }, $row);
+                fputcsv($output, $row);
             }
             exit;
         }
