8f69c6eb418812498d7e8ff51eec6a77925aae24
Vahagn Sookiasian <pars5555@yahoo.com>
2019-01-27 11:21:15 +0400

    123

--- classes/managers/PurseOrderManager.class.php
+++ classes/managers/PurseOrderManager.class.php
@@ -47,6 +47,11 @@ namespace crm\managers {
                 }
                 sleep(1);
             }
+            $content = file_get_contents('https://api.purse.io/api/v1/instant/item/' . $asin);
+            $apiRes = json_decode($content, true);
+            if (isset($apiRes['fiat_price'])) {
+                return floatval($apiRes['fiat_price']);
+            }
             return null;
         }
 
