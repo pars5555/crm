33420e0624341c18474d9f67eb1777924684836f
Vahagn <pars5555@yahoo.com>
2019-05-08 02:32:54 +0400

    123

--- classes/actions/main/warehouse/ExportSalePriceCsvAction.class.php
+++ classes/actions/main/warehouse/ExportSalePriceCsvAction.class.php
@@ -3,19 +3,27 @@
 namespace crm\actions\main\warehouse {
 
     use crm\actions\BaseAction;
+    use crm\managers\ProductCategoryManager;
     use crm\managers\ProductManager;
     use crm\managers\WarehouseManager;
+    use crm\security\RequestGroups;
 
     class ExportSalePriceCsvAction extends BaseAction {
 
         public function service() {
 
             $productsQuantity = WarehouseManager::getInstance()->getAllProductsQuantity();
-            $products = ProductManager::getInstance()->selectAdvance('*',[], 'category_id', 'ASC', null,null,true);
-            $this->exportCsv($products, $productsQuantity);
+            $products = ProductManager::getInstance()->selectAdvance('*', [], 'category_id', 'ASC', null, null, true);
+            $categories = ProductCategoryManager::getInstance()->selectAll();
+            $categoryNamesMappedById = [];
+            foreach ($categories as $category) {
+                $categoryNamesMappedById[$category->getId()] = $category->getName();
+            }
+            $this->exportCsv($products, $productsQuantity, $categoryNamesMappedById);
         }
 
-        public function exportCsv($products, $productsQuantity) {
+        public function exportCsv($products, $productsQuantity, $categoryNamesMappedById) {
+
             header('Content-type: text/csv; charset=UTF-8');
             header('Content-Disposition: attachment; filename=export.csv');
             $output = fopen('php://output', 'w');
@@ -23,9 +31,16 @@ namespace crm\actions\main\warehouse {
 
             //fputcsv($output, ['Item Name', 'Model', 'Quantity','Sale Price']);
             fputcsv($output, ['']);
+            $catId = -1;
             foreach ($products as $product) {
+                if ($catId != $product->getCategoryId()) {
+                    $catId = $product->getCategoryId();
+                    fputcsv($output, []);
+                    fputcsv($output, [$categoryNamesMappedById[$product->getCategoryId()], '', '', '']);
+                }
+
                 if (isset($productsQuantity[$product->getId()]) && $productsQuantity[$product->getId()] > 0) {
-                    $row = [$product->getId(), $product->getName(), $product->getModel(), $productsQuantity[$product->getId()] ?: 0, $product->getSalePrice()?:0];
+                    $row = [$product->getId(), $product->getName(), $product->getModel(), $productsQuantity[$product->getId()] ?: 0, $product->getSalePrice() ?: 0];
                     $row = array_map(function(&$el) {
                         return '="' . $el . '"';
                     }, $row);
@@ -34,9 +49,9 @@ namespace crm\actions\main\warehouse {
             }
             exit;
         }
-        
+
         public function getRequestGroup() {
-            return \crm\security\RequestGroups::$guestRequest;
+            return RequestGroups::$guestRequest;
         }
 
     }
