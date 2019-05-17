a0841eb9540f740622d847e8d60336fc76950c0c
Vahagn <pars5555@yahoo.com>
2019-05-17 05:17:06 +0400

    123

---
+++ classes/actions/api/vanilla/SetCardBalanceAction.class.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * main site action for all ngs site's actions
+ *
+ * @author Levon Naghashyan <levon@naghashyan.com>
+ * @site http://naghashyan.com
+ * @mail levon@naghashyan.com
+ * @year 2009-2014
+ * @package actions.site
+ * @version 6.0
+ * 
+ */
+
+namespace crm\actions\api\vanilla {
+
+    use crm\actions\BaseAction;
+    use crm\managers\VanillaCardsManager;
+    use crm\security\RequestGroups;
+
+    class SetCardBalanceAction extends BaseAction {
+
+        public function service() {
+            $id = intval(NGS()->args()->card_id);
+            $balance = floatval(NGS()->args()->balance);
+            VanillaCardsManager::getInstance()->updateField($id, 'balance', $balance);
+            $this->addParam('success', true);
+        }
+
+        public function getRequestGroup() {
+            return RequestGroups::$guestRequest;
+        }
+
+    }
+
+}
+    
