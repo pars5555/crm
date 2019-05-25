09e6cd1c7d9d081410ca49135889e917b0e9fc85
Vahagn <pars5555@yahoo.com>
2019-05-25 09:23:41 +0400

    123

--- classes/actions/api/vanilla/GetCardToUpdateAction.class.php
+++ classes/actions/api/vanilla/GetCardToUpdateAction.class.php
@@ -28,7 +28,7 @@ namespace crm\actions\api\vanilla {
             $hourAgo = date('Y-m-d H:i:s', strtotime('-1 hour'));
             //$where = array_merge($where, ['AND', 'closed', '=', 0,'AND', 'invalid', '=', 0, 'AND','(','updated_at', '<', "'$hourAgo'",'OR','updated_at', 'IS NULL', ')']);
             $where = array_merge($where, ['AND', 'closed', '=', 0,'AND', 'invalid', '=', 0]);
-            $rows = VanillaCardsManager::getInstance()->selectAdvance('*', $where, 'updated_at', 'DESC', 0, 1);
+            $rows = VanillaCardsManager::getInstance()->selectAdvance('*', $where, 'updated_at', 'ASC', 0, 1);
             if (empty($rows)){
                 $this->addParam('success', true);                
                 $this->addParam('finish', true);    
