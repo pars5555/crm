9515f8836ae9e2e7e2729f881061c63230ea9a4c
Vahagn <pars5555@yahoo.com>
2019-06-09 10:51:42 +0400

    123

--- classes/actions/api/vanilla/SetCardBalanceAction.class.php
+++ classes/actions/api/vanilla/SetCardBalanceAction.class.php
@@ -34,7 +34,7 @@ namespace crm\actions\api\vanilla {
                     VanillaCardsManager::getInstance()->updateField($ccid, 'updated_at', date('Y-m-d H:i:s'));
                     if ($card->getClosed() == 0) {
                         $manager = new \naffiq\telegram\channel\Manager($telegramToken, $telegramCrmChannelId);
-                        $manager->postMessage('****'.substr($card->getNumber(),-6) . ' is closed! last available balance was: $' . floatval(NGS()->args()->balance));
+                        $manager->postMessage('****'.substr($card->getNumber(),-6) . ' is closed! last available balance was: $' . $card->getBalance(). ' initial balance was: '. $card->getInitialBalance());
                         VanillaCardsManager::getInstance()->updateField($ccid, 'closed', 1);
                     }
                 }
