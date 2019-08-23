4c86692a8938deda093ac448ee8e9f39dc581165
Vahagn <pars5555@yahoo.com>
2019-08-23 23:49:52 +0400

    123

--- classes/actions/api/vanilla/GetCardToUpdateAction.class.php
+++ classes/actions/api/vanilla/GetCardToUpdateAction.class.php
@@ -27,7 +27,10 @@ namespace crm\actions\api\vanilla {
             } else {
                 $brandWhere = ['NOT', '(', 'number', 'like', "'5432%'", 'OR', 'number', 'like', "'4847%'", 'OR', 'number', 'like', "'4941%'", ')', 'AND'];
             }
-            $where = array_merge($brandWhere, ['closed', '=', 0, 'AND', 'invalid', '=', 0]);
+            $date = new \DateTime();
+            $date->modify('-5 day');
+            $hourAgo = $date->format('Y-m-d H:i:s');
+            $where = array_merge($brandWhere, ['closed', '=', 0, 'AND', 'invalid', '=', 0, 'AND' , 'created_at','>=',"'$hourAgo'"]);
             $rows = VanillaCardsManager::getInstance()->selectAdvance(['id', 'number', 'month', 'year', 'cvv'], $where, 'updated_at', 'ASC', 0, 1);
             if (empty($rows)) {
                 $this->addParam('success', true);
